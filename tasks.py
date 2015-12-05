d8aa1489a729ad395547217d0c837725633c6f07
Feihong Hsu <feihong.hsu@gmail.com>
2015-12-04 23:36:01 -0600

    Add invoke as a dependency and create the serve task

--- tasks.py
+++ tasks.py
@@ -1,10 +1,13 @@
+import os
 import os.path as op
-import urlparse
+import shutil
+from subprocess import Popen, PIPE
 from mako.template import Template
 from mako.lookup import TemplateLookup
 import bottle
 from livereload import Server
 from livereload.watcher import Watcher
+from invoke import run, task
 
 
 SITE = '/javascript-examples/'
@@ -17,36 +20,44 @@ lookup = TemplateLookup(directories=['templates'])
 
 app = bottle.Bottle()
 
-@app.route('/')
-def index():
-    return '<a href="%s">Go to site</a>' % SITE
+if SITE != '/':
+    @app.route('/')
+    def index():
+        return '<a href="%s">Go to site</a>' % SITE
+
 
 @app.route(SITE)
 @app.route(SITE + '<path:path>')
-def generate(path=''):
+def page(path=''):
     file_ = get_file(path)
-
     if not file_.endswith('.html'):
         return bottle.static_file(path, root='site')
-
     template = Template(open(file_).read(), lookup=lookup, imports=IMPORTS)
     return template.render(site=SITE)
+
+
+@task
+def serve():
+    # bottle.debug(True)
+    # bottle.run(app, host='localhost', port=8000, reloader=True)
+    watcher = Watcher()
+    watcher.watch('site', ignore=lambda p: p.endswith('.babel'))
+    watcher.watch('templates')
+    server = Server(app, watcher)
+    server.serve(port=8000)
+
+
+@task()
+def build():
+    for root, dirs, files in os.walk('site'):
+        for file_ in files:
+            print op.join(root, file_)
 
 
 def get_file(path):
     result = op.join('site', path)
     if op.isfile(result):
         return result
-
     if op.isdir(result) and op.isfile(op.join(result, 'index.html')):
         return op.join(result, 'index.html')
-
     return 'site/404.html'
-
-if __name__ == '__main__':
-    # bottle.debug(True)
-    # bottle.run(app, host='localhost', port=8000, reloader=True)
-    watcher = Watcher()
-    watcher.watch('site', ignore=lambda p: p.endswith('.babel'))
-    server = Server(app, watcher)
-    server.serve(port=8000)
